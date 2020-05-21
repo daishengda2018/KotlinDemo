c32cf0bf7075dcbe06e23e4547019f372f81e4e6
代圣达 <dsd13502@gmail.com>
2020-05-21 16:37:57 +0800

    init repository

---
+++ app/src/main/java/com/daishengda/demo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.daishengda.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun doubleDemo(string: String): Int {
+        return 0
+    }
+}
